696006d198dfb762ec92ec26029d22ff5cb4596d
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-12-20 20:28:57 -0300

    Adicionadas anotações

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
@@ -1,16 +1,34 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
 import me.dio.creditapplicationsystem.enumeration.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
 data class Credit (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    val credtCode: UUID = UUID.randomUUID(),
-    val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFirstStallment: LocalDate,
-    val numberOfInstallments: Int = 0,
+
+    @Column(nullable = false, unique = true) val credtCode: UUID = UUID.randomUUID(),
+
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+
+    @Column(nullable = false) val dayFirstStallment: LocalDate,
+
+    @Column(nullable = false) val numberOfInstallments: Int = 0,
+
+    @Enumerated
     val status: Status = Status.IN_PROGRESS,
+
+    @ManyToOne
     val customer: Customer? = null,
 )
