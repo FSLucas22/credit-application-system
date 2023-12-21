bcb89de01bf3808b0c7c094dd39a586544fdbe72
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-12-20 23:34:47 -0300

    Implementação do endpoint PATCH de clientes

--- src/main/kotlin/me/dio/creditapplicationsystem/controller/CustomerResource.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/controller/CustomerResource.kt
@@ -1,15 +1,10 @@
 package me.dio.creditapplicationsystem.controller
 
 import me.dio.creditapplicationsystem.dto.CustomerDto
+import me.dio.creditapplicationsystem.dto.CustomerUpdateDto
 import me.dio.creditapplicationsystem.dto.CustomerView
 import me.dio.creditapplicationsystem.service.CustomerService
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/customers")
@@ -27,4 +22,14 @@ class CustomerResource(
 
     @DeleteMapping("/{id}")
     fun deleteCustomer(@PathVariable id: Long) = customerService.delete(id)
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long,
+        @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): CustomerView {
+        val customer = customerService.findById(id)
+        val updatedCustomer = customerUpdateDto.toEntity(customer)
+        return CustomerView(customerService.save(updatedCustomer))
+    }
 }
