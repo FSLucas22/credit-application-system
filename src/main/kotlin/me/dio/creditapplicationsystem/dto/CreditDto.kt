2e6f2108cb3019c50ee012420a3a01d57558abb8
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-12-23 00:57:02 -0300

    Validações em CreditDto

--- src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
@@ -1,14 +1,26 @@
 package me.dio.creditapplicationsystem.dto
 
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import me.dio.creditapplicationsystem.entity.Credit
 import me.dio.creditapplicationsystem.entity.Customer
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto (
+    @field:NotNull(message = "creditValue cannot be null")
+    @field:Positive(message = "creditValue must be a positive")
     val creditValue: BigDecimal,
+
+    @field:Future(message = "dayFirstInstallment must be a future date")
     val dayFirstInstallment: LocalDate,
+
+    @field:NotNull(message = "numberOfInstallments cannot be null")
+    @field:Positive(message = "numberOfInstallments must be positive")
     val numberOfInstallments: Int,
+
+    @field:NotNull(message = "customerId cannot be nul")
     val customerId: Long,
 ) {
     fun toEntity(): Credit = Credit(
