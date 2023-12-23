5aab4747ce82eec206a2898bf5aabb9a1a960621
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-12-23 00:46:48 -0300

    Validações em CustomerUpdateDto

--- src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomerUpdateDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomerUpdateDto.kt
@@ -1,15 +1,17 @@
 package me.dio.creditapplicationsystem.dto
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.creditapplicationsystem.entity.Address
 import me.dio.creditapplicationsystem.entity.Customer
 import java.math.BigDecimal
 
 data class CustomerUpdateDto (
-    val firstName: String,
-    val lastName: String,
-    val income: BigDecimal,
-    val zipCode: String,
-    val street: String,
+    @field:NotEmpty(message = "firstName cannot be empty") val firstName: String,
+    @field:NotEmpty(message = "lastName cannot be empty") val lastName: String,
+    @field:NotNull(message = "income cannot be null") val income: BigDecimal,
+    @field:NotEmpty(message = "zipCode cannot be empty") val zipCode: String,
+    @field:NotEmpty(message = "street cannot be empty") val street: String,
 ) {
     fun toEntity(customer: Customer): Customer {
         customer.firstName = firstName
